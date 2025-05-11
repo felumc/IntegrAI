4dc6d917e92ed6530a64b6c485927709d65bfa62
Luis Morales <luismc2211@gmail.com>
2025-05-11 17:28:13 -0600

    fix: remove basePath and assetPrefix for root deployment

--- next.config.js
+++ next.config.js
@@ -6,8 +6,6 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  basePath: isProd ? '/integrai' : '',
-  assetPrefix: isProd ? '/integrai/' : '',
 };
 
 module.exports = nextConfig; 
