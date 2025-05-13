423a60edd0251f2b871e83bd2476e95c5cd014a7
hendruch <hendruchrasgado@gmail.com>
2025-05-12 21:21:36 -0600

    refactor: remove unoptimized images configuration from Next.js settings

--- next.config.js
+++ next.config.js
@@ -2,9 +2,6 @@
 const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig = {
-  images: {
-    unoptimized: true,
-  },
   i18n: {
     locales: ['en', 'es'],
     defaultLocale: 'es',
